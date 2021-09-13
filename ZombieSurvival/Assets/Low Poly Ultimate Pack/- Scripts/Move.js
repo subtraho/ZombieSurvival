863958754b8b63973931951ae9a14a2fa33cc6d8
Niels <389435@student.fontys.nl>
2021-09-13 15:09:43 +0200

    Unitypackages and movement scripts
    Added unity packages in the project and created a player and cameramovement script.

---
+++ ZombieSurvival/Assets/Low Poly Ultimate Pack/- Scripts/Move.js
@@ -0,0 +1,19 @@
+﻿ #pragma strict
+ 
+ public var speed = 1.5;
+ public var spacing = 1.0;
+ private var pos : Vector3;
+ 
+ function Start() {
+     pos = transform.position;
+ }
+ 
+ function Update() {
+    
+     if (Input.GetKeyDown(KeyCode.D))
+         pos.x -= spacing;
+     if (Input.GetKeyDown(KeyCode.A))
+         pos.x += spacing;
+ 
+     transform.position = Vector3.MoveTowards(transform.position, pos, speed * Time.deltaTime);
+ }
