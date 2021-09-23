3343456a72c2032e91934f7d8459b18a47c751b1
Niels <389435@student.fontys.nl>
2021-09-23 14:45:26 +0200

    Small world fixes + stamina
    Implemented stamina and a running mechanic. Also implemented some missing world assets fixing the roads so the player wont fall below the map.

--- ZombieSurvival/Assets/Scripts/PlayerStats.cs
+++ ZombieSurvival/Assets/Scripts/PlayerStats.cs
@@ -9,26 +9,113 @@ public class PlayerStats : MonoBehaviour
     public float hunger;
     public float thirst;
 
+    private WaitForSeconds regenTick = new WaitForSeconds(0.2f);
+    private Coroutine regen;
+
     public void Update()
     {
-        if(health > 100f)
+        UpdateHealth();
+        UpdateStamina();
+        UpdateThirst();
+        UpdateHunger();
+    }
+
+    //healthbar
+
+    public void UpdateHealth()
+    {
+        if (health > 100f)
         {
             health = 100f;
         }
+        else if (health < 0f)
+        {
+            health = 0f;
+        }
+    }
 
+    //stamina bar
+
+    public void UpdateStamina()
+    {
         if (stamina > 100f)
         {
             stamina = 100f;
         }
+        else if(stamina < 0f)
+        {
+            stamina = 0f;
+        }
 
+        if(Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
+        {
+            UseStamina(0.1f);
+        }
+        else
+        {
+            FindObjectOfType<PlayerMovement>().speed = 4f;
+        }
+    }
+
+    public void UseStamina(float amount)
+    {
+        if((stamina - amount) > 0f)
+        {
+            stamina -= amount;
+            FindObjectOfType<PlayerMovement>().speed = 7f;
+
+            if(regen != null)
+            {
+                StopCoroutine(regen);
+            }
+
+            regen = StartCoroutine(RegenStamina());
+        }
+        else
+        {
+            FindObjectOfType<PlayerMovement>().speed = 4f;
+        }
+    }
+
+    private IEnumerator RegenStamina()
+    {
+        yield return new WaitForSeconds(1);
+
+        while(stamina < 100f)
+        {
+            stamina += 0.75f;
+            yield return regenTick;
+        }
+    }
+
+
+
+    ///hunger bar
+
+    public void UpdateHunger()
+    {
         if (hunger > 100f)
         {
             hunger = 100f;
         }
+        else if (hunger < 0f)
+        {
+            hunger = 0f;
+        }
+    }
 
+
+    //thirst bar
+
+    public void UpdateThirst()
+    {
         if (thirst > 100f)
         {
             thirst = 100f;
+        }
+        else if (thirst < 0f)
+        {
+            thirst = 0f;
         }
     }
 }
